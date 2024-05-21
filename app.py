85749adb9d97218f4bd5d8bc6bbbc91342e0b46d
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2024-05-21 16:06:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,17 +17,18 @@ def load_model(gdrive_id='10IOylWbThT3M-pC6eunjh_Z0dc_VfQ-V'):
 tokenizer, model = load_model()
 
 def inference(question, context, tokenizer, model):
+  with torch.no_grad():
     input = tokenizer.encode_plus(context, question, return_tensors='pt')
     res = model(**input)
     start_position = torch.argmax(res.start_logits[0])
     end_position = torch.argmax(res.end_logits[0])
     answer = tokenizer.decode(input['input_ids'][0][start_position:end_position+1], skip_special_tokens=True)
-    return answer
+  return answer
 
 def main():
   st.title('Extractive Question Answering - Machine Reading Comprehension')
   st.title('Model: XLM-RoBERTa-Base. Dataset: SQUAD-V2')
-  context = st.text_input("Context: ", "Hà Nội là thủ đô của nước Cộng hòa Xã hội chủ nghĩa Việt Nam. Hà Nội nằm về phía tây bắc của trung tâm vùng đồng bằng châu thổ sông Hồng, với địa hình bao gồm vùng đồng bằng trung tâm và vùng đồi núi ở phía bắc và phía tây thành phố."
+  context = st.text_input("Context: ", "Hà Nội là thủ đô của nước Cộng hòa Xã hội chủ nghĩa Việt Nam. Hà Nội nằm về phía tây bắc của trung tâm vùng đồng bằng châu thổ sông Hồng, với địa hình bao gồm vùng đồng bằng trung tâm và vùng đồi núi ở phía bắc và phía tây thành phố.")
   question = st.text_input("Question: ", "Thủ đô của nước Việt Nam là gì?")
   result = inference(question, context, tokenizer, model)
   st.success(result) 
